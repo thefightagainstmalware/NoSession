25db82ebeb326de35c3eb35455bcc345dc0f7418
PandaNinjas <admin@malwarefight.wip.la>
2023-07-10 14:11:04 -0400

    Bump ForgeGradle version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 val kotlinVersion = "1.8.21"
 
 dependencies {
-    implementation("com.github.thefightagainstmalware:ForgeGradle:5a1fcb9")
+    implementation("com.github.thefightagainstmalware:ForgeGradle:2a95cb09a6885e391a7dda3c5c5655f7b6779c15")
     implementation("com.github.thefightagainstmalware:MixinGradle:92e66fe")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 }
