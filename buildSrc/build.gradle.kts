e9e69d469e1bd6df190705ee2ae498fcbb475d29
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-09-14 16:16:26 -0700

    among us

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 val kotlinVersion = "1.8.21"
 
 dependencies {
-    implementation("com.github.thefightagainstmalware:ForgeGradle:63f19a0")
+    implementation("com.github.thefightagainstmalware:ForgeGradle:77c50e0")
     implementation("com.github.thefightagainstmalware:MixinGradle:92e66fe")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 }
