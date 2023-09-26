d496de3e25964fa06a603d6662dd682427ff0bb3
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-09-25 18:42:05 -0700

    Fix typo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,6 @@ import org.apache.tools.ant.taskdefs.condition.Os
 if (Os.isFamily(Os.FAMILY_UNIX) && !Os.isFamily(Os.FAMILY_MAC)) {
   include("nosession_libc")
 }
-if (Os.isFamily(Os.FAMILY_WINDOWS) {
+if (Os.isFamily(Os.FAMILY_WINDOWS)) {
   include("chromium_bindings")
 }
