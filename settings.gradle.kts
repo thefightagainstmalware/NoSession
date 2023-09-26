0f1e209d9541c268581c7de7a58b2e7893eb8618
Jocelyn Castellano <admin@malwarefight.wip.la>
2023-09-25 18:40:49 -0700

    Make inclusion conditional

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
-include("nosession_libc")
-include("chromium_bindings")
+import org.apache.tools.ant.taskdefs.condition.Os
+
+if (Os.isFamily(Os.FAMILY_UNIX) && !Os.isFamily(Os.FAMILY_MAC)) {
+  include("nosession_libc")
+}
+if (Os.isFamily(Os.FAMILY_WINDOWS) {
+  include("chromium_bindings")
+}
