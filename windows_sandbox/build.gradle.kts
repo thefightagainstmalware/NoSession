2dc03011a9e3c55ff24de491c266ad8a070d5685
PandaNinjas <admin@malwarefight.wip.la>
2023-10-12 09:47:19 -0700

    Moved WindowsSandbox.java to the windows_sandbox subproject

--- windows_sandbox/build.gradle.kts
+++ windows_sandbox/build.gradle.kts
@@ -1,10 +1,12 @@
+@file:Suppress("UnstableApiUsage")
+
 import org.apache.tools.ant.taskdefs.condition.Os
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
 
 plugins {
     `cpp-library`
     `java-base`
-
+    java
 }
 val lib: Configuration by configurations.creating
 
@@ -40,23 +42,25 @@ project.afterEvaluate {
             compilerArgs.add("/wd4668") // 4668: macro is not defined
             compilerArgs.add("/wd4820") // 4820: 'bytes' bytes padding added after construct 'member_name'
             compilerArgs.add("/wd4710") // 4710: function not inlined
-            compilerArgs.add("/wd4711") // 4711: function is inlined. there is no winning with with msvc
+            compilerArgs.add("/wd4711") // 4711: function is inlined. there is no winning with msvc
+            compilerArgs.add("/wd5045") // 5045: spectre mitigation is enabled
             compilerArgs.add("/std:c++17")
             compilerArgs.add("/EHa")
         }
     }
-
     val linkRelease: AbstractLinkTask by tasks
-    val archive by tasks.creating(Zip::class) {
-        archivesName.set("archive.jar")
+
+    tasks.withType(Jar::class) {
+        this.dependsOn.add(linkRelease)
+        archivesName.set("archive")
         destinationDirectory.set(layout.buildDirectory.dir("libs"))
         from(linkRelease.linkedFile) {
             val targetPlatform = linkRelease.targetPlatform.get()
             into("native/" + when(val arch = targetPlatform.architecture.name) {
                 "x86-64" -> "amd64"
                 else -> arch
-            } + "/" + targetPlatform.operatingSystem.name)
+                // os.name is not used because it returns something specific to the os version, e.g. "Windows 8", "Windows 11"
+            } + "/windows")
         }
     }
-    artifacts.add(lib.name, archive)
 }
